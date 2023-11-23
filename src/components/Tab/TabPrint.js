abb2807eea74806d2949666a6d291f36138c85e2
YeWon Seo <syw1236@naver.com>
2023-11-23 09:35:36 +0900

    Update TabPrint.js

--- src/components/Tab/TabPrint.js
+++ src/components/Tab/TabPrint.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import festivalsData from '../../data/festivalsData';
-import Tab from './Tab';
-import Tab_Winter from './Tab_Winter';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import festivalsData from "../../data/festivalsData";
+import Tab from "./Tab";
+import Tab_Winter from "./Tab_Winter";
 import NavigationBar from "../CommonComp/navigationBar";
 
-//Tab.js와 Tab_Winter.js 컴포넌트를 출력하는 전체 컴포넌트 TabPrint.js
+//Tab.js 컴포넌트를 출력하는 전체 컴포넌트 TabPrint.js
 const TabPrint = () => {
-
   return (
     <div>
-       <NavigationBar backgroundColor={"rgb(206, 220, 255)"} hoverColor={"rgb(145, 176, 255)"} />
-       <Tab festivalsData={festivalsData} />
-       <Tab_Winter festivalsData={festivalsData} />
+      <NavigationBar
+        backgroundColor={"rgb(206, 220, 255)"}
+        hoverColor={"rgb(145, 176, 255)"}
+      />
+      <Tab festivalsData={festivalsData} />
     </div>
   );
-}
+};
 
 export default TabPrint;
