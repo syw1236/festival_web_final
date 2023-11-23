4a02a147492083339bc71e663b125a5d6d1b2a95
YeWon Seo <syw1236@naver.com>
2023-11-23 09:35:34 +0900

    Update Tab_Winter.js

--- src/components/Tab/Tab_Winter.js
+++ src/components/Tab/Tab_Winter.js
@@ -1,17 +1,17 @@
 // Tab_Winter.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../../css/Tab/Tab_Winter.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../css/Tab/Tab_Winter.css";
 
 const Tab_Winter = ({ festivalsData }) => {
   const [currentTab, clickTab] = useState(0);
 
   //메뉴 탭 구현 (탭 이름과 각 탭에 들어갈 내용)
   const menuArr = [
-    { name: '봄 축제', content: '#철쭉 #꽃' },
-    { name: '여름 축제', content: '#머드 #물놀이' },
-    { name: '가을 축제', content: '#가을 #단풍 #핑크뮬리' },
-    { name: '2023을 장식할 겨울 축제', content: '#눈 #얼음분수' },
+    { name: "봄 축제", content: "#철쭉 #꽃" },
+    { name: "여름 축제", content: "#머드 #물놀이" },
+    { name: "가을 축제", content: "#가을 #단풍 #핑크뮬리" },
+    { name: "2023을 장식할 겨울 축제", content: "#눈 #얼음분수" },
   ];
 
   //선택한 인덱스로 clickTab함수를 이용하여 탭 이동
@@ -21,10 +21,10 @@ const Tab_Winter = ({ festivalsData }) => {
 
   //각 탭마다 정해진 키워드를 이용
   const filteredFestivals = festivalsData.filter((festival) => {
-    const tab0Keywords = ['철쭉'];
-    const tab1Keywords = ['머드'];
-    const tab2Keywords = ['단풍', '가을', '핑크뮬리'];
-    const tab3Keywords = ['눈꽃','얼음분수'];
+    const tab0Keywords = ["철쭉"];
+    const tab1Keywords = ["머드"];
+    const tab2Keywords = ["단풍", "가을", "핑크뮬리"];
+    const tab3Keywords = ["눈꽃", "얼음분수"];
 
     const keywords =
       currentTab === 0
@@ -50,7 +50,11 @@ const Tab_Winter = ({ festivalsData }) => {
           {menuArr.map((el, index) => (
             <li
               key={index}
-              className={index === currentTab ? 'winter-submenu active' : 'winter-submenu'}
+              className={
+                index === currentTab
+                  ? "winter-submenu active"
+                  : "winter-submenu"
+              }
               onClick={() => selectMenuHandler(index)}
             >
               {el.name}
@@ -58,8 +62,16 @@ const Tab_Winter = ({ festivalsData }) => {
           ))}
         </ul>
         <div className="winter-desc">
-        <p style={{ fontWeight: 'bold', fontSize: '25px' }}>{menuArr[currentTab].content}</p>
-          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          <p style={{ fontWeight: "bold", fontSize: "25px" }}>
+            {menuArr[currentTab].content}
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
             {filteredFestivals.map((item) => (
               <div className="winter-festival-card" key={item.id}>
                 <p>{item.name}</p>
